a8d9db12db9d82fbda78f9958953b317cecac281
Saurabhrana2290 <saurabhrana532@gmail.com>
2021-09-14 23:13:44 +0530

    Updated navbar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import './App.css';
-import { Container, Navbar, Nav, Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        
-        <Navbar collapseOnSelect bg="myViolet" variant="light">
 
-          <Navbar.Brand href="#home">bre.ad/jane</Navbar.Brand>
-          <Navbar.Toggle id="responsive-navbar" aria-controls="responsive-navbar-nav" />
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" >bre.ad/jane</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
 
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
-              <Nav.Item>
-                <Button bsStyle="secondary">Book Trial</Button>
-              </Nav.Item>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-
-      </Container>
-
-
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
 
       <h1>Hello World!</h1>
     </div>
